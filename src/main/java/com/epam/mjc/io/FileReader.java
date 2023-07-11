68b090e22ffdca5d56817df1b2e53c13e8b92a9d
Кононов Алексей <aleksey.n.kononov@gazprombank.ru>
2023-07-11 21:20:16 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        int age = 0;
+        String email = null;
+        Long phone = null;
+
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Name:")) {
+                    name = line.substring(5).trim();
+                } else if (line.startsWith("Age:")) {
+                    age = Integer.parseInt(line.substring(4).trim());
+                } else if (line.startsWith("Email:")) {
+                    email = line.substring(6).trim();
+                } else if (line.startsWith("Phone:")) {
+                    phone = Long.parseLong(line.substring(6).trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
